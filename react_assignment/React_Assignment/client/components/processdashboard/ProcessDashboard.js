3a3498b7fcb2ad9c27fffc2c86bac895704a5ede
Ankit <ankit.aggarwal6@wipro.com>
2018-08-07 00:22:52 +0530

    updated code

--- react_assignment/React_Assignment/client/components/processdashboard/ProcessDashboard.js
+++ react_assignment/React_Assignment/client/components/processdashboard/ProcessDashboard.js
@@ -8,46 +8,47 @@ import Collapse from '@material-ui/core/Collapse';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import StarBorder from '@material-ui/icons/StarBorder';
 
-
 class ProcessDashboard extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {open:true};
-		this.state ={data:["RTD","Time-mizer","J-Fill Handheld","Wal-Mart Solution Center","Command Centre","Tim Hortons-3-button Dispenser","Sink-Mizer Plastic"]}
-	}	
-	
-	handleClick = ()=>{
-		this.setState(state => ({open:!state.open}));
-	}
-	render(){
-		console.log(this,"this");
-		var that = this
-		var menuList = this.state.data;
-		var index=0
-		return (	
-			<List component="nav">
-			 { menuList.map(function(menuitem){
-				index++;
-				return <div><ListItem button onClick={that.handleClick}>
+        constructor(props){
+                super(props);
+                this.state = {open:true};
+                this.state ={data:["RTD","Time-mizer","J-Fill Handheld","Wal-Mart Solution Center","Command Centre","Tim Hortons-3-button Dispenser","Sink-Mizer Plastic"]}
+        }
+
+        handleClick = ()=>{
+                this.setState(state => ({open:!state.open}));
+        }
+        render(){
+                console.log(this,"this");
+                var that = this
+                var menuList = this.state.data;
+                var index=0
+                return (
+                        <List component="nav">
+                         { menuList.map(function(menuitem){
+                                index++;
+                                return <div><ListItem button onClick={that.handleClick}>
                                                  <ListItemText key={index} inset primary={menuitem}/>
-							{that.state.open?<ExpandLess/> : <ExpandMore/>}
-                                            	</ListItem>
+                                                        {that.state.open? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                                                </ListItem>
                                                 <Collapse in={that.state.open} timeout="auto" unmountOnExit>
                                                         <List component="div" disablePadding>
                                                                 <ListItem button>
                                                                 </ListItem>
                                                         </List>
                                                 </Collapse>
-					</div>
+                                        </div>
 
-			})}
-			</List>
+                        })}
+                        </List>
 
-		)
-	
-	
-	}
+                )
+
+
+        }
 }
-export default ProcessDashboard 
+export default ProcessDashboard
+
