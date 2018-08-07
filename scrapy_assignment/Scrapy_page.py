7448e39cfc0c1d9fa7427941b9c980daf454cf6b
Ankit <ankit.aggarwal6@wipro.com>
2018-08-07 13:20:39 +0530

    Adding python code again

--- scrapy_assignment/Scrapy_page.py
+++ scrapy_assignment/Scrapy_page.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import os
+import re
 
 class pensylvania:#(Spider):
     name = 'pensylvania'
@@ -15,51 +16,47 @@ class pensylvania:#(Spider):
 
     def parse(self):#,response):
             	self.driver.get('https://www.pafoodsafety.pa.gov/Web/Inspection/PublicInspectionSearch.aspx')
-            #while self.Done:
              	try:
                     next = self.driver.find_element_by_xpath('//*[@id="MainContent_btnSearch"]')
                     next.click()
                     url = 'https://www.pafoodsafety.pa.gov/Web/Inspection/PublicInspectionSearch.aspx'
-		    #print "#########################################################################"
 		    html = self.driver.page_source
-		    #print html
 		    soup_level1=BeautifulSoup(html,"lxml")
-		    #print soup_level1
 		    datalist = [] #empty list
 		    x = 0 #counter
 		    #print dir(soup_level1)
 		    for table in soup_level1.find_all('table',id="MainContent_gvInspections"):
 			#print dir(table)
 			rows = table.findAll('tr')
+			#matches = ([nonformattednameaddress]+="[]")\w+
+			regex = re.compile(r'''((nonformattednameaddress+="(.*?)")+)''')
+			matches = regex.findall(str(rows))
+			print type(matches)
+			for x in matches:
+				print "############################"
+				print x[0].split('=')[1]
+			print len(matches)
+			matchesVal = [x[0].split('=')[1] for x in matches]
+			df1 = pd.DataFrame(matchesVal,columns=['Name / Address'])
+			df1.drop_duplicates(inplace=True)
 			#inputTags =  rows.find(attrs={"name":"nonformattednameaddress"})
-			#print inputTags
-			#print output = [x['nonformattednameaddress'] for x in inputTags]
-			df = pd.read_html(str(table),header=0)
-			print len(df)
-			print df
-			for dt in df:
-			    print "***********************************************************"
+			#df = pd.read_html(str(table),header=0) # this was giving all the data but format was not good 
+			#print len(df)
+			#print df
+			#for dt in df:
+			    #print "***********************************************************"
 			    #dt.columns
-			    dt.dropna(inplace=True,axis=1) 
+			    #dt.dropna(inplace=True,axis=1) 
 			    #print dt
-			    print dt.columns
-			    with open('mydata.csv','a') as f: 
-			        dt[['Name / Address']].to_csv('mydata.csv',sep=',',header=True,index=False,mode='a')
-				break
-		    #print dir(self.driver.page_source)
-		    #print "this is the type######################"
-		    #print type(html)
-		    #print html
+			    #print dt.columns
+			    #with open('mydata.csv','a') as f: 
+			df1[['Name / Address']].to_csv('mydata.csv',sep=',',header=True,index=False,mode='a')
                     #yield Request(url,callback=self.parse2)
                 except Exception,e:
 		    print e
-                    #break
 	        self.Done=False	
 	        self.driver.close()
 if __name__=="__main__":
     Scrap = pensylvania();
     Scrap.parse()
 
-    #def parse2(self,response):
-	#print "You are here******************************"
-        #print response.body
